d03834b71cf302a48d059fe123df65a03cb1bcc7
Erdenemunkh <ts.erka@gmail.com>
2018-01-31 23:33:29 +0800

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { BinanceApiClient } from "binance-api-client";
+
+const client1 = new BinanceApiClient("", "");
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
+    client1.getAccountData().then(res => {
+      console.log(res);
+    });
   }
-
 }
